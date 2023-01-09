72a47cbbb5a9afa04746a43904e73e4898bc11f8
vidhanshu <vidhanshu.borade.cs@ghrce.raisoni.net>
2023-01-09 19:45:37 +0530

    base uri for the image serving changed

--- src/configs/env.js
+++ src/configs/env.js
@@ -1,16 +1,16 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
+const BASE_URI = "https://vibe-m39i.onrender.com/";
+// const BASE_URI = "http://192.168.43.35:5000/";
 const DATABASE_HOST = process.env.DATABASE_HOST;
 const DATABASE_USERNAME = process.env.DATABASE_USERNAME;
 const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
 const DATABASE_NAME = process.env.DATABASE_NAME;
 const JWT_SECRET = process.env.JWT_SECRET;
-const PROFILE_IMAGE_SERVING_URL =
-  "http://192.168.43.35:5000/user/profile/image";
-const POST_IMAGE_SERVING_URL = "http://192.168.43.35:5000/post";
-const STATUS_IMAGE_SERVING_URL = "http://192.168.43.35:5000/status";
-
+const PROFILE_IMAGE_SERVING_URL = `${BASE_URI}user/profile/image`;
+const POST_IMAGE_SERVING_URL = `${BASE_URI}post`;
+const STATUS_IMAGE_SERVING_URL = `${BASE_URI}status`;
 module.exports = {
   DATABASE_HOST,
   DATABASE_USERNAME,
